1c01069ce3d8186f5305e990f270fa488a4460b4
Rolf Erik Lekang <me@rolflekang.com>
2017-10-09 13:40:35 +0200

    fix: Remove all non enzyme use cases
    BREAKING CHANGE: This will break all usages that is sending in jsx
    directly. See readme for correct usages.

--- readme.md
+++ readme.md
@@ -21,24 +21,6 @@ expect(<div><Super /></div>).to.have.component(Super)
 expect(<div><Super /></div>).to.not.have.component(Super)
 ```
 
-### Old usage
-This type of usage is deprecated and will result in a warning. This will be removed in version 3.
-
-```javascript
-import chai from 'chai'
-import chaiHaveReactComponent from "chai-have-react-component"
-
-chai.use(chaiHaveReactComponent)
-
-const component = TestUtils.renderIntoDocument(<div><Super /></div>);
-
-// it should pass
-expect(component).to.have.component(Super);
-
-// should fail with "Expected 'div' to not have component 'Super'"
-expect(component).to.not.have.component(Super);
-```
-
 ## Found a bug?
 Please open a pr with a broken test.
 
--- src/index.js
+++ src/index.js
@@ -1,66 +1,15 @@
 /* eslint-env browser */
-import { haveXpath, getFindDOMNode } from 'have-xpath'
-import paramCase from 'param-case'
-
-let React
 let enzyme
-let findDOMNode = findDOMNode || (global && global.findDOMNode) // eslint-disable-line no-use-before-define, max-len
-
-function haveReactComponent(component, subComponent) {
-  return React.addons.TestUtils.scryRenderedComponentsWithType(component, subComponent).length > 0
-}
-
-const create14Message = className => `
-      Be aware that since you are using react 0.14.x this library is
-      falling back looking for a element with a class name "${className}".
-      More info about the fallback can be found at
-      https://github.com/relekang/chai-have-react-component
-`
 
 export default function haveComponent(Chai) {
-  let counter = 0
-
   Chai.Assertion.addMethod('component', function evaluateComponent(component) {
     if (typeof enzyme === 'undefined') {
       enzyme = require('enzyme')
     }
-    if (typeof React === 'undefined') {
-      React = require('react')
-      if (/0\.13/.test(React.version)) {
-        React = require('react/addons')
-      }
-    }
-
-    if (this._obj.hasOwnProperty('_context') || this._obj.hasOwnProperty('_owner')) {
-      return this.assert(
-        enzyme.mount(this._obj).find(component).length > 0,
-        `Expected "${this._obj}" to have component '${component.displayName}'`,
-        `Expected "${this._obj}" to not have component '${component.displayName}'`
-      )
-    }
-
-    counter++
-    findDOMNode = findDOMNode || getFindDOMNode()
-
-    if (/0\.13/.test(React.version)) {
-      const dom = this._obj.getDOMNode().outerHTML
-      this.assert(
-        haveReactComponent(this._obj, component),
-        `Expected "${dom}" to have component '${component.displayName}'`,
-        `Expected "${dom}" to not have component '${component.displayName}'`
-      )
-    } else if (/^(16\.)|(15\.)|(0\.14)/.test(React.version)) {
-      const dom = findDOMNode(this._obj).outerHTML
-      const className = paramCase(component.displayName)
-      const extra = create14Message(className)
-      this.assert(
-        haveXpath(this._obj, `//*[contains(@class, '${className}')]`),
-        `Expected ${dom} to have component '${component.displayName}' ${extra}`,
-        `Expected ${dom} to not have component '${component.displayName}' ${extra}`
-      )
-    } else {
-      throw new Error('This version of react is not supported')
-    }
-    return null
+    return this.assert(
+      enzyme.mount(this._obj).find(component).length > 0,
+      `Expected "${this._obj}" to have component '${component.displayName}'`,
+      `Expected "${this._obj}" to not have component '${component.displayName}'`
+    )
   })
 }
